1c8fece6dbda7d9716f9dea1838a261261a1a742
muhrenaldi <121756437+muhrenaldi@users.noreply.github.com>
2023-01-03 10:38:03 +0700

    nambahin terbit dll
    udah

--- website/main.js
+++ website/main.js
@@ -29,8 +29,10 @@ function getJadwalSholat() {
     const jadwal = data.jadwal.data;
     document.querySelector('.imsak').textContent = jadwal.imsak;
     document.querySelector('.subuh').textContent = jadwal.subuh;
-    document.querySelector('.dzuhur').textContent = jadwal.dzuhur;
+    document.querySelector('.terbit').textContent = jadwal.terbit;
+    document.querySelector('.dhuha').textContent = jadwal.dhuha;
     document.querySelector('.ashar').textContent = jadwal.ashar;
+    document.querySelector('.dzuhur').textContent = jadwal.dzuhur;
     document.querySelector('.maghrib').textContent = jadwal.maghrib;
     document.querySelector('.isya').textContent = jadwal.isya;
     document.querySelector('.tanggal').textContent = jadwal.tanggal;
