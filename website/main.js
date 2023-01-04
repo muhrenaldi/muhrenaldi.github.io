95c9cd296cc6f4a1a5e40437551133b73be1b991
muhrenaldi <121756437+muhrenaldi@users.noreply.github.com>
2023-01-04 13:35:59 +0700

    tambah input kota dll
    okss

--- website/main.js
+++ website/main.js
@@ -22,8 +22,11 @@ function hari(){
 
 const tanggal = `${getYear}-${bulan()}-${hari()}`;
 
+const tampilKota = document.querySelector('.judul-kota');
+tampilKota.textContent = localStorage.judulkota
+
 function getJadwalSholat() {
-  fetch('https://api.banghasan.com/sholat/format/json/jadwal/kota/679/tanggal/' + tanggal)
+  fetch('https://api.banghasan.com/sholat/format/json/jadwal/kota/' + parseInt(localStorage.idkota) + '/tanggal/' + tanggal)
   .then(response => response.json())
   .then(data => {
     const jadwal = data.jadwal.data;
@@ -38,5 +41,57 @@ function getJadwalSholat() {
     document.querySelector('.tanggal').textContent = jadwal.tanggal;
   });
 }
+
+let inputSearch = document.getElementById('inputSearch')
+const cardList = document.getElementsByClassName('card-list')[0]
+
+
+inputSearch.addEventListener('keyup',function(){
+  let valueSearch = inputSearch.value.length;
+
+  if(valueSearch > 0) {
+    cardList.classList.remove('hidden-list');
+
+    fetch('https://api.banghasan.com/sholat/format/json/kota')
+      .then(response => response.json())
+      .then(response => {
+        const kota = response.kota;
+        let listKota = '';
+        kota.forEach( k => {
+          listKota += `<a href="#" data-idkota="${k.id}" id="nama-kota">${k.nama}</a><br>`;
+        });
+        const namaKota = document.getElementsByClassName('card-list')[0];
+        namaKota.innerHTML = listKota;
+
+        const isiKota = document.querySelectorAll('#nama-kota');
+        isiKota.forEach( kota => {
+
+          const filterText = inputSearch.value.toLowerCase();
+          const itemText = kota.firstChild.textContent.toLowerCase();
+
+          if(itemText.indexOf(filterText) != -1){
+            kota.setAttribute("style", "display: block");
+          } else {
+            kota.setAttribute("style", "display: none !important");
+          }
+
+          kota.addEventListener('click', function(){
+            const idkota = this.dataset.idkota;
+            const judulkota = this.textContent;
+            window.localStorage.setItem('idkota', idkota);
+            window.localStorage.setItem('judulkota', judulkota);
+            namaKota.classList.add('hidden-list');
+            inputSearch.value = '';
+            location.reload();
+          });
+        });
+
+        // console.log(kota)
+      });
+  } else {
+    cardList.classList.add('hidden-list');
+  }
+
+});
 
 getJadwalSholat();
